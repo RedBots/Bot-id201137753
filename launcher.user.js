0210285034bc90f0733007466a78b07a622cf6e6
RedBots <rednelss@ya.ru>
2015-08-27 14:21:52 +0500

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -1299,7 +1299,7 @@ console.log("Running Bot Launcher!");
                 dArc = [],
                 dText = [],
                 lines = [],
-                names = ["youtube.com/KyKyRyZaa"],
+                names = ["KyKyRyZaa TV"],
                 originalName = names[Math.floor(Math.random() * names.length)],
                 sessionScore = 0,
                 serverIP = "",
